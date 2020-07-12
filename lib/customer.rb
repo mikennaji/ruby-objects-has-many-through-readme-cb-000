110c02b7f7a685884177976037b4e15d0503bc58
Michael Nnaji <mnnaji3@uwo.ca>
2020-07-12 20:53:40 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,37 @@
 class Customer
-  
+
+attr_accessor :name, :age
+
+@@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter,total,tip)
+    Meal.new(waiter,self,total,tip)
+  end
+
+  def meals
+    Meal.all.select{|x| x .customer === self}
+  end
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+
+  end
+
+
 end
